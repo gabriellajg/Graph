693ee508b23247de19fca2f6fe9149902ed84d5f
gabriellajg <7454640+gabriellajg@users.noreply.github.com>
2022-07-16 22:50:38 -0500

    major push for streamlit deployment

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This dashboard provides analyses of product reviews in terms of: 
 
 1. Exploratory Data Analysis (EDA)
-2. Sentiment detection (Positive, Neutral, Negative)
+2. Sentiment detection (POS, NEU, NEG)
 3. Keywords extraction
---
+++ dashboard_streamlit/app.py
@@ -0,0 +1,282 @@
+import streamlit as st
+from streamlit_chat import message
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import collections
+import plotly.express as px
+import time
+from wordcloud import WordCloud
+from PIL import Image
+import gensim
+from gensim.parsing.preprocessing import STOPWORDS
+
+# ---- HEADER ---
+
+st.set_page_config(page_title="Extracting Sentiment and Insights from Product Reviews",
+                   layout="wide")
+
+t1, t2 = st.columns((0.07,1))
+t1.image('LEGO_logo.svg.png', width = 100)
+t2.title("Extracting Sentiment and Insights from Product Reviews")
+
+
+# ---- FUNCTIONS ---
+
+@st.cache
+def color_sentiment(sentiment):
+    colors = {
+        "POS": "yellow",
+        "NEU": "green",
+        "NEG": "red"
+    }
+    return f"background-color: {colors[sentiment]}"
+
+@st.cache
+def get_allItems(df, allTriple):
+	return df[df.set_name.isin(allTriple.item.unique())].set_name.unique()
+
+@st.cache
+def get_triples_by_product(selected_products):
+	#return allTriple[allTriple.item.isin(selected_products) & allTriple.BERT_SentLabel.isin(selected_emotions)]
+	return allTriple[allTriple.item.isin(selected_products)].reset_index(drop=True)
+
+@st.cache
+def get_df_by_product(selected_products, selected_emotions=['POS','NEU','NEG']):
+	return df[df.set_name.isin(selected_products) & df.BERT_SentLabel.isin(selected_emotions)].reset_index(drop=True)
+
+@st.cache(suppress_st_warning=True)
+def plot_pos_wordcloud(pos_keywords, out = 'target'):
+    if out == 'target':
+        cm = ' '.join(list(map(str, pos_keywords)))
+        mask = np.array(Image.open('upvote.png'))
+        wcP = WordCloud(stopwords=stop_words, width=3000, height=2000, random_state=1, collocations=False,
+                        background_color="white", colormap='rainbow', mask=mask).generate(cm)
+        return wcP
+    else:
+        filtered_words = [word for word in ' '.join(pos_keywords).split() if word not in stop_words]
+        top_word = collections.Counter(filtered_words).most_common(1)[0][0]
+        temp = triple_selected[triple_selected.sentiment == 'POS'][[top_word in w for w in triple_selected[triple_selected.sentiment == 'POS'].target]]
+        cm = ' '.join(list(map(str, temp.opinion)))
+        wcP = WordCloud(stopwords=stop_words, collocations=False, background_color="white").generate(cm)
+        return wcP, top_word
+
+
+@st.cache(suppress_st_warning=True)
+def plot_neg_wordcloud(neg_keywords, out = 'target'):
+    if out == 'target':
+        cm = ' '.join(list(map(str, neg_keywords)))
+        mask = np.array(Image.open('downvote.png'))
+        wcN = WordCloud(stopwords=stop_words, width=2000, height=1500, random_state=1, collocations=False,
+                        background_color="white", colormap='rainbow', mask=mask).generate(cm)
+        return wcN
+    else:
+        filtered_words = [word for word in ' '.join(neg_keywords).split() if word not in stop_words]
+        top_word = collections.Counter(filtered_words).most_common(1)[0][0]
+        temp = triple_selected[triple_selected.sentiment == 'NEG'][[top_word in w for w in triple_selected[triple_selected.sentiment == 'NEG'].target]]
+        cm = ' '.join(list(map(str, temp.opinion)))
+        wcN = WordCloud(stopwords=stop_words, collocations=False, background_color="white").generate(cm)
+        return wcN, top_word
+
+@st.cache(suppress_st_warning=True)
+def plot_neu_wordcloud(neu_keywords, out = 'target'):
+    if out == 'target':
+        cm = ' '.join(list(map(str, neu_keywords)))
+        wc = WordCloud(stopwords=stop_words, width=3000, height=2000, collocations=False,
+                       background_color="white", colormap='rainbow').generate(cm)
+        return wc
+    else:
+        filtered_words = [word for word in ' '.join(neu_keywords).split() if word not in stop_words]
+        top_word = collections.Counter(filtered_words).most_common(1)[0][0]
+        temp = triple_selected[triple_selected.sentiment == 'NEU'][[top_word in w for w in triple_selected[triple_selected.sentiment == 'NEU'].target]]
+        cm = ' '.join(list(map(str, temp.opinion)))
+        wc = WordCloud(stopwords=stop_words, collocations=False, background_color="white").generate(cm)
+        return wc, top_word
+
+
+
+# ---- IMPORT DATA ---
+
+df = pd.read_csv("df_running.csv")
+allTriple = pd.read_csv("allTriple0_5200.csv")
+
+
+
+# ---- SIDEBAR ---
+#st.sidebar.header("Please Make Selections Here:")
+
+st.markdown('### 1. Select A Product:')
+selected_products = [st.selectbox(
+    label = 'Products sorted by counts of reviews:',
+    options=get_allItems(df, allTriple)
+)]
+
+df_selected = get_df_by_product(selected_products)
+triple_selected = get_triples_by_product(selected_products)
+
+# ---- MAINPAGE ---
+
+Rating = round(df_selected.overallRating.mean(), 2)
+valueForMoney = round(df_selected.valueForMoney.mean(), 2)
+overallPlayExperience = round(df_selected.overallPlayExperience.mean(), 2)
+SentScore = round(df_selected.sentiment.mean(), 2)
+NumTriples = triple_selected.reviewID.count()
+NumReviews = df_selected.comment.shape[0]
+#st.write(Rating, valueForMoney, overallPlayExperience, NumReviews, n, SentScore)
+
+col1, col2 , col3 , col4 , col5 = st.columns(5)
+
+with col1:
+    st.markdown("##### Product Name")
+    st.markdown(f"##### 🧱{selected_products[0]}")
+
+with col2:
+    st.markdown('##### Numerical Ratings')
+    st.markdown(f"##### :star:{Rating}")
+
+with col3:
+    st.markdown("##### BERT Sentiment Score")
+    st.markdown(f"##### :smiley:{SentScore}")
+
+with col4:
+    st.markdown("##### Number of Reviews")
+    st.markdown(f"##### :speech_balloon:{NumReviews}")
+
+with col5:
+    st.markdown("##### Number of Keywords")
+    st.markdown(f"##### :bulb:{NumTriples}")
+
+st.markdown('''---''')
+
+# ---- Sentiment ---
+
+st.markdown('### 2. Select the Sentiment(s):')
+selected_emotions = st.multiselect(
+    label = 'POS: Positive; NEU: Neutral; NEG: Negative',
+    options= ['POS', 'NEU', 'NEG'],
+    default = ['POS', 'NEU', 'NEG']
+)
+
+df_emo_selected = get_df_by_product(selected_products, selected_emotions)
+
+st.markdown('#### Product Reviews')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.dataframe(df_emo_selected.loc[:,['comment','sentiment','BERT_SentLabel']])
+
+with col2:
+    checked = st.checkbox(
+        "Show detailed reviews and sentiment scores (0-1)",
+        key="idI"
+    )
+    if checked:
+        for i in range(min(2, NumReviews)):
+            message(df_emo_selected.comment[i], key=f"rv{i}")
+            message(str(df_emo_selected.sentiment[i]), is_user=True, key=f"sent{i}")
+
+    checked2 = st.checkbox(
+        "Show More (2-5)",
+        key="idX"
+    )
+    if checked2 and NumReviews>2:
+        for i in range(2, min(6, NumReviews)):
+            message(df_emo_selected.comment[i], key=f"rv2{i}")
+            message(str(df_emo_selected.sentiment[i]), is_user=True, key=f"sent2{i}")
+        message("......")
+
+st.markdown('''---''')
+
+# ---- An example ---
+
+st.markdown('### 3. Keywords Extraction (A Quick Demo)')
+
+t1,t2,t3 = st.columns((0.07,1,0.07))
+t2.image('Presentation1.png', use_column_width = True,clamp = True)
+
+
+st.markdown('''---''')
+
+# ---- Keywords Extraction ---
+
+st.markdown('### 4. Insights Dashboard')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown('#### Extracted Keywords')
+    st.dataframe(triple_selected.loc[:,['item','target','opinion','sentiment']].style.applymap(
+        color_sentiment,
+        subset=["sentiment"]
+    ))
+
+with col2:
+    st.markdown('#### Distribution of Sentiment of Keywords')
+    long_df = pd.DataFrame(triple_selected.sentiment.value_counts()).reindex(['POS', 'NEU', 'NEG']).reset_index()
+    long_df = long_df.rename(columns={'index': 'Sentiment', 'sentiment': 'Counts'})
+    fig = px.bar(long_df, x="Sentiment", y="Counts", color="Sentiment")
+    fig.update_layout(barmode='relative')
+    fig
+
+# ---- Word Cloud ---
+
+
+pos_keywords = [k for k in triple_selected[triple_selected.sentiment=='POS'].target]#.extend([k for k in allTriple[allTriple.sentiment=='POS'].opinion])
+neg_keywords = [k for k in triple_selected[triple_selected.sentiment=='NEG'].target]#.extend([k for k in allTriple[allTriple.sentiment=='NEG'].opinion])
+neu_keywords = [k for k in triple_selected[triple_selected.sentiment=='NEU'].target]#.extend([k for k in allTriple[allTriple.sentiment=='NEU'].opinion])
+stop_words = ["lego", "legos", "set", "sets", "build", "building", "built", "piece", "pieces", "batman", "batmobile", "really", "completely"] + list(STOPWORDS) + selected_products[0].lower().split()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.markdown("<h4 style='text-align: center'>Positive Reviews Mentioned:</h4>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.imshow(plot_pos_wordcloud(pos_keywords), aspect='auto')
+    ax.axis('off')
+    st.pyplot(fig)
+
+with col2:
+    st.markdown("<h4 style='text-align: center'>Negative Reviews Mentioned:</h4>", unsafe_allow_html=True)
+    #st.markdown('#### Negative Reviews Mentioned:')
+    fig, ax = plt.subplots()
+    ax.imshow(plot_neg_wordcloud(neg_keywords))
+    ax.axis('off')
+    st.pyplot(fig)
+
+with col3:
+    st.markdown("<h4 style='text-align: center'>Neutral Reviews Mentioned:</h4>", unsafe_allow_html=True)
+    #st.markdown('#### Neutral Reviews Mentioned:')
+    fig, ax = plt.subplots()
+    ax.imshow(plot_neu_wordcloud(neu_keywords))
+    ax.axis('off')
+    st.pyplot(fig)
+
+col1, col2 , col3 = st.columns(3)
+
+with col1:
+    st.markdown("<h4 style='text-align: center'>Opinions of Top Highlight:</h4>", unsafe_allow_html=True)
+    wc, top_word = plot_pos_wordcloud(pos_keywords, out = 'opinion')
+    st.markdown(f"<h5 style='text-align: center'>{top_word}</h5>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.imshow(wc, interpolation='nearest')
+    ax.axis('off')
+    st.pyplot(fig)
+
+with col2:
+    st.markdown("<h4 style='text-align: center'>Opinions of Top Concern:</h4>", unsafe_allow_html=True)
+    wc, top_word = plot_neg_wordcloud(neg_keywords, out = 'opinion')
+    st.markdown(f"<h5 style='text-align: center'>{top_word}</h5>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.imshow(wc, interpolation='nearest')
+    ax.axis('off')
+    st.pyplot(fig)
+
+with col3:
+    st.markdown("<h4 style='text-align: center'>Opinions of Average Feature</h4>", unsafe_allow_html=True)
+    wc, top_word = plot_neu_wordcloud(neu_keywords, out = 'opinion')
+    st.markdown(f"<h5 style='text-align: center'>{top_word}</h5>", unsafe_allow_html=True)
+    fig, ax = plt.subplots()
+    ax.imshow(wc, interpolation='nearest')
+    ax.axis('off')
+    st.pyplot(fig)
